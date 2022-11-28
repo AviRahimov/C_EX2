ebdc909e5909cb93345260d67a9aced4d99428a9
Avi Rahimov <rahimovavi3@gmail.com>
2022-11-28 23:08:36 +0200

    change

--- main.c
+++ main.c
@@ -1,16 +1,6 @@
 #include <stdio.h>
 #include "my_mat.h"
 
-int mat1[10][10] = {{0, 3, 1, 0, 0, 2, 0, 0, 0, 0},
-                    {3, 0, 1, 0, 0, 0, 0, 0, 0, 0},
-                    {1, 1, 0, 0, 0, 0, 0, 0, 0, 0},
-                    {0, 0, 0, 0, 0, 0, 0, 5, 0, 0},
-                    {0, 0, 0, 0, 0, 0, 0, 4, 1, 1},
-                    {2, 0, 0, 0, 0, 0, 2, 0, 0, 0},
-                    {0, 0, 0, 0, 0, 2, 0, 0, 0, 0},
-                    {0, 0, 0, 5, 4, 0, 0, 0, 0, 2},
-                    {0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
-                    {0, 0, 0, 0, 1, 0, 0, 2, 0, 0}};
 int main()
 {
     int i, j, p;
@@ -19,7 +9,6 @@ int main()
 
     while (c != 'D')
     {
-        printf("Enter a letter: \n");
         scanf("%c", &c);
         switch (c)
         {
@@ -28,7 +17,6 @@ int main()
         {
             for (int j = 0; j < 10; j++)
             {
-                printf("enter the value for cell [%d %d]", i, j);
                 scanf("%d", &p);
                 mat[i][j] = p;
             }
@@ -39,14 +27,12 @@ int main()
             break;
 
         case 'B':
-            printf("write i and j indexes you want to find of there is a pth between them: ");
             scanf("%d %d", &i, &j);
             isPath(mat, i, j);
             printf("\n");
             break;
 
         case 'C':
-            printf("write i and j indexes you want to explore the path for them: ");
             scanf("%d %d", &i, &j);
             Shortest_path(mat, i, j);
             printf("\n");
